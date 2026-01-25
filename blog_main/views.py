13955480d3aa132dece691b253d7285648aa0fa6
Vignesh21J <vignesh11212003@gmail.com>
2026-01-25 22:15:53 +0530

    feat: about users and their follows done

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from blogs.models import Category
 from blogs.models import Blog
 
+from users.models import About
+
 def home(request):
     featured_posts = Blog.objects.filter(is_featured = True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False, status='Published')
 
+    # Fetch about us
+    try:
+        about = About.objects.first()
+    except:
+        about = None
+
     context = {
         'featured_posts':featured_posts,
-        'posts':posts
+        'posts':posts,
+        'about':about
     }
     return render(request, 'home.html', context)
