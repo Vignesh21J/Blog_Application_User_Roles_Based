78d53bf3b56588cf83707da96ae396e6dadccac5
Vignesh21J <vignesh11212003@gmail.com>
2026-01-25 16:49:11 +0530

    feat: implement category-based blog listing page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    # Fetch the posts that belongs to the category with the id category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+
+    # category = Category.objects.get(pk=category_id)
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
